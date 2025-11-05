5e8f1ae4a5294f7bedfd6141bb178e2b9eb1742a
tuongxuyxa2709-lang <tuongxuyxa2709@gmail.com>
2025-11-05 19:27:23 +0700

    Update Untitled15.cpp

--- Untitled15.cpp
+++ Untitled15.cpp
@@ -3,17 +3,17 @@
 int main() {
     int a, b;
 
-    // Nh?p hai s? nguyên
+    
     printf("Nhap hai so nguyen a va b: ");
     scanf("%d %d", &a, &b);
 
-    // Ki?m tra c? hai s? có l?n hon 0 hay không (dùng toán t? &&)
+   
     int ca_hai_lon_hon_0 = (a > 0) && (b > 0);
 
-    // Ki?m tra ít nh?t m?t trong hai s? là s? ch?n (dùng toán t? ||)
+    
     int it_nhat_mot_chan = (a % 2 == 0) || (b % 2 == 0);
 
-    // In k?t qu?
+   
     printf("\n=== Ket qua kiem tra ===\n");
 
     if (ca_hai_lon_hon_0)
@@ -28,4 +28,5 @@ int main() {
 
     return 0;
 }
+
 
