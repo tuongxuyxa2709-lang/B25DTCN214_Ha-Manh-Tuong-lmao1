56cc74d16bc170aca5607b10f0810e4685945823
tuongxuyxa2709-lang <tuongxuyxa2709@gmail.com>
2025-11-05 19:24:29 +0700

    Update Untitled17.cpp

--- Untitled17.cpp
+++ Untitled17.cpp
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    // Khai báo các bi?n v?i ki?u d? li?u khác nhau
-    int a = 10;        // Bi?n nguyên (integer)
+   
+    int a = 10;        // Bi?n nguyÃªn (integer)
     float b = 3.14;    // Bi?n th?c (float)
-    char c = 'A';      // Bi?n kı t? (character)
+    char c = 'A';      // Bi?n kÄ± t? (character)
 
-    // In ra giá tr? c?a các bi?n
+ 
     printf("Gia tri cua bien a (int): %d\n", a);
     printf("Gia tri cua bien b (float): %.2f\n", b);
     printf("Gia tri cua bien c (char): %c\n", c);
 
     return 0;
 }
+
 
